7a33c0f52f23698e11d12693eb6d601c2facad18
Samir-Zade <samirzadeofficial@gmail.com>
2024-04-24 12:11:41 +0530

    dagshub updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## This is my machine learning project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/Samir-Zade/MLProject.mlflow \
+MLFLOW_TRACKING_USERNAME=Samir-Zade \
+MLFLOW_TRACKING_PASSWORD=88a145d7cd009190546c3907380d38d02750bb88 \
+python script.py
--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -5,6 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 import os, sys
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 
 #models
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
@@ -28,6 +31,12 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
@@ -95,6 +104,44 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+
+            print("This is the best model: ")
+            print(best_model_name)
+
+            model_name = list(params.keys())
+            
+            actual_model = ""
+
+            for model in model_name:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
+
+            #mlflow code
+
+            mlflow.set_registry_uri("https://dagshub.com/Samir-Zade/MLProject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            with mlflow.start_run():
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(predicted_qualities, y_test)
+
+                mlflow.log_params(best_params)
+                
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                #model registry does not work with file store
+                if tracking_url_type_store != "file":
+                    
+                    #register the model
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name = actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
 
             #trreshold
             if best_model_score < 0.6:
